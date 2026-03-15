d72d6df60d88b0f7a116237fd2202c811ccfec59
Oghenetejiri29 <aomr0001@algonquinlive.com>
2026-03-15 15:18:31 -0400

    Initial commit

---
+++ greet_world.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+Program name : greet_world.py
+Program purpose: Greet the world
+Author: Oghenetejiri ,041267831, Section: 021
+Date & Version: 27-01-2026, version 1.0
+Completion Time: 1 hour 
+"""
+
+#Greet the world
+
+print ("Hello Ms. Amal")
